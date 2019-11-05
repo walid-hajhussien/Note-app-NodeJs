49e486d79db276b061c17e43cf5a90f13b8e4b6a
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-11-05 14:56:53 +0200

    (fix) doplicate notes

--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const chalk = require("chalk");
 
 // get notes
 function getNotes() {
@@ -9,10 +10,15 @@ function getNotes() {
 
 function addNote(title, body) {
   const notes = loadNotes();
+  const doplicate = notes.length === 0 ? false : isDoplicateNotes(title, notes);
 
-  notes.push({ title: title, body: body });
-
-  saveNotes(notes);
+  if (doplicate) {
+    console.log(chalk.red("Doplicate Notes can't saved!"));
+  } else {
+    notes.push({ title: title, body: body });
+    saveNotes(notes);
+    console.log(chalk.green("Note Saved..."));
+  }
 }
 
 // helper
@@ -29,6 +35,14 @@ function loadNotes() {
 function saveNotes(notes) {
   const notesJson = JSON.stringify(notes);
   fs.writeFileSync("notes.json", notesJson);
+}
+
+function isDoplicateNotes(title, notes) {
+  const isDoplicate = notes.filter(function(note) {
+    return note.title === title;
+  });
+
+  return isDoplicate.length === 0 ? false : true;
 }
 
 module.exports = {
