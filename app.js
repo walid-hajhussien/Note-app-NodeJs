7ac24098781c292b8153d1a9169bfaff9740de04
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-11-05 11:09:41 +0200

    regester the method in the yargs

--- app.js
+++ app.js
@@ -1,4 +1,56 @@
+// to validate the string
 const validator = require("validator");
+// to color the console
 const chalk = require("chalk");
+// to get input from the console
 const yargs = require("yargs");
-console.log(yargs.argv);
+
+// regester the remove command
+yargs.command({
+  command: "remove",
+  describe: "remove note",
+  handler: function() {
+    console.log("the note removed!");
+  }
+});
+
+// regester the read command
+yargs.command({
+  command: "read",
+  describe: "read note",
+  handler: function() {
+    console.log("the note readed!");
+  }
+});
+
+// regester the list command
+yargs.command({
+  command: "list",
+  describe: "list note",
+  handler: function() {
+    console.log("the note listed!");
+  }
+});
+
+// regester the add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "title of the note",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "note body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: function(argv) {
+    console.log("Adding a new note!", argv.title, "//" + argv.body);
+  }
+});
+
+yargs.parse();
