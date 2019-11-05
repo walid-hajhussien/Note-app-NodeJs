b312e16c24939addcd500868b4e9999a04b3cb61
walid-hajhussien <eng.waleed_com@yahoo.com>
2019-11-05 09:15:44 +0200

    add the yargs library to deal with args

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 const validator = require("validator");
 const chalk = require("chalk");
-
-const isEmail = validator.isEmail("1@yahoo.com");
-
-if (isEmail) {
-  console.log(chalk.bold.green("Success!"));
-} else {
-  console.log(chalk.red("Faild!"));
-}
+const yargs = require("yargs");
+console.log(yargs.argv);
